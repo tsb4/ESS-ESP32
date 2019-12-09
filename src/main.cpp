94b7ee799f74685f4d384df905796090d63cd60e
Tiago Barros <tsb4@cin.ufpe.br>
2019-12-08 22:53:00 -0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@
 #include<string.h>
 #include<time.h>
 #include<ctime>
-
-time_t tt;
  
  
 
@@ -35,6 +33,10 @@ void setup()
   
   pinMode(led_red, OUTPUT);
   pinMode(led_green, OUTPUT);
+  digitalWrite(led_green, HIGH);
+  digitalWrite(led_red, HIGH);
+  delay(5000);
+  digitalWrite(led_red, LOW); 
   Serial.begin(9600);
   
   delay(4000);
@@ -46,44 +48,11 @@ void setup()
   }
  
   Serial.println("Connected to the WiFi network");
-  digitalWrite(led_green, HIGH);
-  digitalWrite(led_red, HIGH);
-  delay(5000);
-  digitalWrite(led_green, LOW); 
+  
 }
  
 void loop()
 {
-  /* if(state==0){
-    //espera pelo comando para fazer a autenticação do lixo
-    //Ao fazer, state=1
-    if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
- 
-      HTTPClient http;
-  
-      http.begin("http://jsonplaceholder.typicode.com/comments?id=10"); //Specify the URL
-      int httpCode = http.GET();                                        //Make the request
-  
-      if (httpCode > 0) { //Check for the returning code
-  
-        String payload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(payload);
-        String teste = String("oi");
-        if(payload.equals(teste)) state=1;
-        else state = 0;
-
-      }
-  
-      else {
-        Serial.println("Error on HTTP request");
-        state = 0;
-      }
-  
-      http.end(); //Free the resources
-    }
-    
-  }*/ 
   if(state==0){
     digitalWrite(led_red, HIGH);
     digitalWrite(led_green, LOW);
@@ -96,27 +65,32 @@ void loop()
       delay(100);
     }
     Serial.println("Foi");
+    digitalWrite(led_green, HIGH);
     state=1;
   }
   if(state==1){
+    digitalWrite(led_red, LOW);
+    delay(500);
+    digitalWrite(led_red, HIGH);
     //Tenta mandar de volta a autenticação
     if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
 
       HTTPClient http;   
     
-      http.begin("http://192.168.43.119:3000/confirmarQuentinha"); 
-      //http.begin("https://cin.ufpe.br/~tsb4/temp/");  //Specify destination for HTTP request
-      http.addHeader("Content-Type", "text/plain");             //Specify content-type header
+      http.begin("http://192.168.43.61:3000/confirmarQuentinha"); 
+      http.addHeader("Content-Type", "text/plain");
     
-      int httpResponseCode = http.POST("POSTING from ESP32");   //Send the actual POST request
+      int httpResponseCode = http.POST("POSTING from ESP32");
     
       if(httpResponseCode>0){
     
-        String response = http.getString();                       //Get the response to the request
+        String response = http.getString();                      
     
-        Serial.println(httpResponseCode);   //Print return code
-        Serial.println(response);           //Print request answer
+        Serial.println(httpResponseCode);   
+        Serial.println(response);           
         state=0;
+        digitalWrite(led_red, LOW);
+        delay(2000);
     
       }else{
     
